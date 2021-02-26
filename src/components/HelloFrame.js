d761fd7e506409c036ad1170daf519a7805c44e6
benibene <benedikt.kupfer@online.de>
2021-02-27 00:35:35 +0100

    HelloFrame added

---
+++ src/components/HelloFrame.js
@@ -0,0 +1,87 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function HelloFrame() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.textView}>
+                <Text style={styles.gutenMorgenText}>
+                    {timeExpression()}
+                    {" Julian" + ","}
+                </Text>
+                <Text style={styles.freieFahrtText}>
+                    {"Freie Fahrt"}
+                </Text>
+                <Text style={styles.ankuftszeitText}>
+                    {"Deine geschätzte Ankunftszeit ist\n"}
+                    {"Heute um 12:24"}
+                </Text>
+            </View>
+            <TouchableOpacity style={styles.buttonRouteAnzeigen}>
+                <Text>
+                    {"Route Anzeigen"}
+                </Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 200,
+        overflow: "visible",
+        backgroundColor: "#a8dbff",
+        borderRadius: 30,
+        justifyContent:'center',
+        alignItems: 'center',
+    },
+
+    textView: {
+        position: 'absolute',
+        bottom: 60,
+    },
+
+    gutenMorgenText:{
+        overflow: "hidden",
+        color: "#000000",
+        fontStyle: "normal",
+        fontSize: 20,
+    },
+
+    freieFahrtText:{
+        overflow: "hidden",
+        color: "#000000",
+        fontSize: 37,
+        fontStyle: "normal",
+    },
+
+    ankuftszeitText:{
+        overflow: "hidden",
+        color: "#000000",
+        fontStyle: "normal",
+    },
+
+    buttonRouteAnzeigen:{
+        height: 10,
+        backgroundColor: "#4fcfe8",
+        padding: 20,
+        borderRadius: 50,
+        justifyContent:'center',
+        alignItems:'center',
+        position: 'absolute',
+        bottom: 20,
+        right: 20,
+    },
+});
+
+const timeExpression = () => {
+    let time = new Date().getHours;
+    if(time < 12){
+        return "Guten Morgen";
+    }else if(time<18){
+        return "Guten Tag";
+    }else{
+        return "Guten Abend";
+    }
+}
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,10 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Container from '../components/layout/Container';
+import ContainerBoxed from '../components/layout/ContainerBoxed';
+import HelloFrame from '../components/HelloFrame';
 
 export default function HomeScreen() {
     return (
-        <Container></Container>
+        <Container>
+            <ContainerBoxed>
+                <HelloFrame/>
+            </ContainerBoxed>
+        </Container>
     );
 }
 
